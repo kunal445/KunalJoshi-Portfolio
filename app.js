e02623fe227ec10fab6eb5f8b82b625c22fce921
kunal445 <73367589+kunal445@users.noreply.github.com>
2022-04-25 09:36:26 -0400

    made the hamburger menu close when you click on the link

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 
 const hamburger = document.querySelector('.hamburgerMenu');
-const ul = document.querySelector('.headerLinks');
+const ul = document.querySelector('.headerMenu');
 
 hamburger.addEventListener("click", () => {
     ul.classList.toggle('showMe');
 })
+
+document.querySelectorAll('.headerLink').forEach(link => link.addEventListener('click', () => {
+    ul.classList.remove("showMe");
+}))
